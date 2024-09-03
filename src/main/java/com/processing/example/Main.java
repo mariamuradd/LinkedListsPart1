0b23bff523ea3e3791d46a7374844c89153a7b5c
mariamuradd <mmmurad@smu.edu>
2024-09-03 18:11:19 -0500

    first commit

---
+++ src/main/java/com/processing/example/Main.java
@@ -0,0 +1,10 @@
+package com.processing.example;
+
+import processing.core.*;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("com.processing.example.Main");
+        System.out.println("Hello world!");
+    }
+}
